f9ec7b5577a13d2a412c18001975c280edcb9f29
arctusadem <bruno.cesar.salgado@terra.com.br>
2022-03-06 23:14:49 -0800

    bubble sort

---
+++ sorting-algorithms/src/BubbleSort.java
@@ -0,0 +1,24 @@
+public class BubbleSort {
+
+    public static void bubbleSort (int[] array) {
+        int i = 0;
+        int j = array.length-1;
+        int x;
+
+        for (; i < array.length; i++){
+            for (; j > i; j--){
+                if (array[j] < array[j-1]) {
+                    x = array[j-1];
+                    array[j-1] = array[j];
+                    array[j] = x;
+                }
+            }
+            j = array.length-1;
+        }
+
+        for (int a: array) {
+            System.out.println(a);
+        }
+    }
+
+}
---
+++ sorting-algorithms/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        String lb = "\n ----------------------------------------------------------------- \n";
+        int disorderedArray[] = { 5, 9, 2, 7, 12, 6, 15, 14, 1, 3, 13, 18, 8, 10, 20, 7, 4, 11, 19, 17 };
+        /*
+        int length = 100;
+        int disorderedArray[] = new int[length];
+
+        for (int i = 0; i < length; i++)
+            disorderedArray[i] = (int)Math.round(Math.random() * 100);
+        */
+        
+        for (int a: disorderedArray)
+            System.out.println(a);
+
+        System.out.println(lb);
+
+	    BubbleSort.bubbleSort(disorderedArray);
+    }
+}
