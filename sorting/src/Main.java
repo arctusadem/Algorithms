34695364a4ef0c1af387567be231140440c4ecde
arctusadem <bruno.cesar.salgado@terra.com.br>
2022-03-07 01:50:34 -0800

    insertion sort

--- sorting/src/Main.java
+++ sorting/src/Main.java
@@ -17,6 +17,7 @@ public class Main {
 
         System.out.println(lb);
 
-	    BubbleSort.bubbleSort(disorderedArray);
+	    //BubbleSort.bubbleSort(disorderedArray);
+        InsertionSort.insertionSort(disorderedArray);
     }
 }
